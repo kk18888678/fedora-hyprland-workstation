c6962ce29de5b0b6cca66b50e10e2c15291d7918
KK <kk18888678@gmail.com>
2026-08-31 16:34:36 +0530

    Add baseline Hyprland window rules

---
+++ dotfiles/hypr/windowrules.lua
@@ -0,0 +1,64 @@
+-- Hyprland window rules.
+--
+-- Keep this file focused on generic workstation behaviour.
+-- Application-specific rules can be added when those applications
+-- become part of the workstation manifest.
+
+-- Audio control
+
+hl.window_rule({
+    match = {
+        class = "^org\\.pulseaudio\\.pavucontrol$",
+    },
+    float = true,
+})
+
+-- Prevent the display from sleeping while fullscreen media is playing.
+
+hl.window_rule({
+    match = {
+        class = "^(.*mpv.*|.*vlc.*)$",
+    },
+    idle_inhibit = "fullscreen",
+})
+
+-- Prevent idle while supported browsers are fullscreen.
+
+hl.window_rule({
+    match = {
+        class = "^(.*chromium.*|.*firefox.*|.*brave.*|.*ulaa.*)$",
+    },
+    idle_inhibit = "fullscreen",
+})
+
+-- Picture-in-picture windows.
+
+hl.window_rule({
+    match = {
+        title = "^([Pp]icture[-\\s]?[Ii]n[-\\s]?[Pp]icture).*$",
+    },
+    tag = "picture-in-picture",
+})
+
+hl.window_rule({
+    match = {
+        tag = "picture-in-picture",
+    },
+    float = true,
+    pin = true,
+    keep_aspect_ratio = true,
+    move = "73% 72%",
+    size = "25% 25%",
+})
+
+-- Some JetBrains IDE helper windows should not steal initial focus.
+
+hl.window_rule({
+    match = {
+        class = "^.*jetbrains.*$",
+        title = "^win[0-9]+$",
+    },
+    no_initial_focus = true,
+})
+
+return true
